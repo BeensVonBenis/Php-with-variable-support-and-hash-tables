ba9bb52feec3eef2e3bab6b95efa9e865cecf8c9
Beens von Benis Kowalikov <145481307+BeensVonBenis@users.noreply.github.com>
2025-12-07 21:02:41 +0100

    Added edge case test

---
+++ testDataCollisions.php
@@ -0,0 +1,20 @@
+<?php;
+
+$permutacje=[
+    "abc"=>"1",
+    "acb"=>"2",
+    "cab"=>"3",
+    "bba"=>"4",
+    "cac"=>"5",
+    "aca"=>"6",
+    "bbb"=>"7",
+    "bcb"=>"8",
+];
+echo $permutacje["abc"];
+echo $permutacje["acb"];
+echo $permutacje["cab"];
+echo $permutacje["bba"];
+echo $permutacje["cac"];
+echo $permutacje["aca"];
+echo $permutacje["bbb"];
+echo $permutacje["bcb"];
